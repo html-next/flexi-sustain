e0589bbc5daeaf894359429be6e4556daa38dc75
Robert Wagner <rwwagner90@gmail.com>
2017-06-13 19:33:51 -0400

    Update Ember

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,10 +1,11 @@
 /* eslint-env node */
+'use strict';
 
 module.exports = function(environment) {
-  var ENV = {
+  let ENV = {
     modulePrefix: 'dummy',
     podModulePrefix: 'dummy/routes',
-    environment: environment,
+    environment,
     rootURL: '/',
     locationType: 'hash',
     EmberENV: {
